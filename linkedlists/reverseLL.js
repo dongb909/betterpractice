6a45201cd98ebec39268a570a76b7343d1db2fa5
dongb909 <dongb909@gmail.com>
2020-04-05 16:44:11 -0700

    Reverse linkedlist

--- linkedlists/reverseLL.js
+++ linkedlists/reverseLL.js
@@ -1,4 +1,4 @@
-/*Reverse a singly linked list.
+/*Reverse a singly linked head.
 
 Example:
 
@@ -19,5 +19,44 @@ A linked list can be reversed either iteratively or recursively. Could you imple
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    
+    let prev = null; //is pretty much the head of the reversed list
+    while (head){
+      //keep track of next node before unlinking the current node
+      let next = head.next;
+      //link current node to the reversed list
+      head.next = prev;
+      //make the new node the new head of the reversed list
+      prev = head;
+      //set current node to the next node to work on  aka head
+      head = next;
+    }
+    return head;
 };
+
+
+/*var reverseList = function(head) {
+    let prevNode = null
+    while (head != null) {
+        let nextNode = head.next
+        head.next = prevNode
+        prevNode = head
+        head = nextNode
+    }
+    return prevNode
+}; */
+
+/*var reverseList = function(head) {
+    
+    let prev = null
+    let current = head
+    
+    while (current !== null) {
+        let nextNode = current.next
+        current.next = prev
+        prev = current
+        current = nextNode
+    }
+    
+    return prev
+    
+}; */
