c00f28693956de5af65f934901812741c6f31ced
dongb909 <dongb909@gmail.com>
2020-05-14 18:46:46 -0700

    pathsum

--- trees/invertTree.js
+++ trees/invertTree.js
@@ -4,11 +4,11 @@ Example:
 
 Input:
 
-     4
-   /   \
-  2     7
+     4                      5             5
+   /   \                  /   \         /    \      just swap chilren of 5
+  2     7                3    7         7    3        BFS you're storing in Q so bettter to use DFS bc can store in stack with indices
  / \   / \
-1   3 6   9
+1   3 6   9           null null null
 Output:
 
      4
@@ -29,9 +29,31 @@ but you can’t invert a binary tree on a whiteboard so f*** off. */
  * }
  */
 /**
- * @param {TreeNode} root
+ * @param {TreeNode} root       9 is left subtree of 7 etc
  * @return {TreeNode}
  */
-var invertTree = function(root) {
+
+ //edge: if only 1 child and other is null, if only 1 root node aka no children, no root, 
+var invertTree = function(root) {  //7
+  if (!root) return root ; //just used to stop recursion but not storing anything anywhere
+  // if (!root.left && !root.right) //ignore bc if both null, when swapped is some thing
+  //same with if one child is null and other is value bc still swapping so only care about swapping
+  if(!root.left && !root.right) {//this allows it to skip the last step of the leaf node
+    //good for when you have a lot of leaf nodes. if not then it makes th ecode more ugly
+    return root
     
+  }
+  invertTree(root.left)   // 9
+  invertTree(root.right)   //6
+  const temp = root.left
+  root.left = root.right
+  root.right = temp  
+  return root;    //returning the inverted tree at that root
 };
+
+//inverting tree in place so don't have to return anything bc just working on the reference of the tree
+//depends on the expection of the solution
+
+var invertTree = function(root) {
+
+}
--- trees/pathSum.js
+++ trees/pathSum.js
@@ -1,4 +1,4 @@
-/*Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+/*Given a binary tree and a sum, determine if the tree has a root-to-leaf path such thatrue adding up all the values along the path equals the given sum.
 
 Note: A leaf is a node with no children.
 
@@ -27,5 +27,28 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22. */
  * @return {boolean}
  */
 var hasPathSum = function(root, sum) {
-    
-};
+     // if (sum < 0) return false; //don't need tjos and next line bc there's only 1 true condition
+      // if(!root && sum > 0) return false;
+      //if(!root && sum === 0) return true;
+    if(!root) return false; //for the very first root node
+      //NOT if(sum === 0) return true
+    if (!root.left && !root.right){ //for the very last leaf node
+         //to not move on to next node and also still need to evaluate this node, plus only care about sum at leaf node, not any other node along the way
+        return sum - root.val === 0
+    } 
+      //already know this node won't produce a 0
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum-root.val) //traverse until hit leaf node and then evaluate at leaf, calculating along the way
+  };
+  
+
+
+
+
+
+console.log(true && false)
+console.log(true && true)
+     console.log(false&& false)
+          console.log(true || false)
+               console.log(true || true)
+                    console.log(false|| false)
+                    console.log(false || true)
