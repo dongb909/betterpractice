1596fd96f5e28496845c40a5ab6cdb4d4246ef60
dongb909 <dongb909@gmail.com>
2020-06-01 19:51:58 -0700

    optimize OOP

--- trees/lruCache.js
+++ trees/lruCache.js
@@ -10,143 +10,122 @@
 //output = last 100 to be stored in a data store for quick access
 //access by id
 
-// class Node(val) {
-//     this.val = val;
-//     this.prev = null;
-//     this.next = null;
-// }
+class Node{
+    constructor(key,val){
+        this.key = key
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
 
-// class List {
-//     this.head = null;
-//     this.tail = null;
-// }
+class List {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
 
-// class Orders (capacity) {
-//     this.orders = new Map();
-//     this.list = new List();
-//     this.capacity = capacity;
+    isEmpty(){
+        return !this.head && !this.tail;
+        // !true
+        // !false
+    }
+
+    append(node){
+        //always adding to end but be aware of the prev and next props
+        if (this.isEmpty()) this.list.head = this.list.tail = node;
+    }
+
+    detach(node){
+    //deleting the node completely while linking the neighbors
+
+    }
+}
+
+class LRUcache {
+    constructor(capacity){
+        this.keys = new Map();
+        this.list = new List();
+        this.capacity = capacity;
+    }
     
-//     access(id){
-//         if (!this.orders.get(id)) return null;
-//         return this.orders.get(id);
-//     }
-    
-//     add(id, zip){
-//         //empty list to create list and map
-//         let node = new Node(zip);
-//         if (this.orders.size() === 0) {
-//             this.orders.set(id, node);
-//             this.list.head = node;
-//             this.list.tail = node;
-//             return;
-//         }
-//         //check if id valid then take out and add to end with new zip
-//         if (this.orders.get(id)) {
-//             let nodeToDel = this.orders.get(id);
-//             if(nodeToDel === this.list.head && nodeToDel ===this.list.tail){
-//                 nodeToDel.val = node.val;
-//             } else if(nodeToDel === this.list.head) {
-//                 this.list.head = this.list.head.next;
-//                 nodeToDel.next = null;
-//                 this.list.head.prev = null;
-//             } else if (nodeToDel === this.list.tail) {
-//                 nodeToDel.val = node.val;
-//             } else {
-//                 nodeToDel.prev.next = nodeToDel.next;
-//                 nodeToDel.next.prev = nodeToDel.prev;
-//                 //need to remember if there's only 1 node or think about deleting on both ends and middle
-//                 //write down what your variables are at each point in time when running through
-//             }
-//             this.orders.delete(id);
-//             this.orders.set(id, node);
-//             this.list.tail.next(node);
-//             node.prev = this.list.tail;
-//             this.list.tail = this.list.tail.next;
-//             return;
-//         }
-//         //if id not valid then check map size if > capacit the dequeue enqueue
-//         else {
-//             if (this.orders.size() < this.capacity) {
-//                 this.orders.set(id, node);
-//                 this.list.tail.next(node);
-//                 node.prev = this.list.tail;
-//                 this.list.tail = this.list.tail.next;
-//                 return;
-//             } else {
-//                 let nodeToDel = this.list.head;
-//                 nodeToDel.next.prev = null;
-//                 this.list.head = this.list.head.next;
-//                 this.orders.delete(need to have id in node for this case);
-//                 this.orders.set(id, node);
-//                 this.list.tail.next(node);
-//                 node.prev = this.list.tail;
-//                 this.list.tail = this.list.tail.next;
-//                 return;
-//             }
-//         }
-//     }
-// }
+    access(key){
+        if (!this.keys.get(key)) return null;
+        return this.keys.get(key);
+    }
+    isEmpty(){
+        return this.keys.size() === 0;
+    }
+
+    add(key, zip){ //add shouldn't even know that there's a linkedlist at all.
+        //empty list to create list and map
+        let node = new Node(key, zip);
+
+        //check if key valkey then take out and add to end with new zip
+        if (this.keys.get(key)) {
+            let toDeta = this.keys.get(key);
+            if(nodeToDel === this.list.head && nodeToDel === this.list.tail){ //only 1 node
+                nodeToDel.val = node.val;
+            } else if(nodeToDel === this.list.head) { // delete node from wherever is same process
+                this.list.head = this.list.head.next; // add node is always to end no matter where it was previously
+                nodeToDel.next = null;
+                this.list.head.prev = null;
+            } else if (nodeToDel === this.list.tail) {
+                nodeToDel.val = node.val;
+            } else {
+                nodeToDel.prev.next = nodeToDel.next;
+                nodeToDel.next.prev = nodeToDel.prev;
+                //need to remember if there's only 1 node or think about deleting on both ends and mkeydle
+                //write down what your variables are at each point in time when running through
+            }
+            this.keys.delete(key);
+            this.keys.set(key, node);
+            this.list.tail.next(node);
+            node.prev = this.list.tail;
+            this.list.tail = this.list.tail.next;
+            return;
+        }
+        //if key not valkey then check map size if > capacit the dequeue enqueue
+        else {
+            if (this.keys.size() < this.capacity) {
+                this.keys.set(key, node);
+                this.list.tail.next(node);
+                node.prev = this.list.tail;
+                this.list.tail = this.list.tail.next;
+                return;
+            } else {
+                let nodeToDel = this.list.head;
+                nodeToDel.next.prev = null;
+                this.list.head = this.list.head.next;
+                this.keys.delete(need to have key in node for this case);
+                this.keys.set(key, node);
+                this.list.tail.next(node);
+                node.prev = this.list.tail;
+                this.list.tail = this.list.tail.next;
+                return;
+            }
+        }
+    }
+    addToHead(node){
+
+    }
+    addToTail(node){
+
+    }
+    moveToTail(node){
+
+    }
+    deleteNode(node){
+
+    }
+}
 
 
 // make it dry,
 // need to find the mistakes when going through examples on your own
 // should really think about edge cases prior
-// you didn't see the zip on line 38
+// you dkeyn't see the zip on line 38
 // and then seeing the edgecases
 // you were explaining your ASSUMPTION of what's happening not what actually happened!
 // you were testing your own logic with assumptions.
 
-
-
-// Given a string of characters, find the first character that occurs only once.
-
-
-
-// babacbcdddef
- 
-/*is it just a-z
-are there spaces? how do you want to handle spaces
-what types of characters and are there any to omit?
-have to handle empty string? how to handle, like what to return? what's the expected output if we have an empty string
-if alpha, casing?
-return single char string
-input is a string, valid string -clarify
-first char that occurs only once means WHAT
-    only 1 count for that char in entire string
-    or meaning not in a repeat
-    ********how handle if all are repeating chars . am i guaranteed that there's a character that occurs only once?
-    *****what does first mean? going left to right or right to left?
-
-
-are there time and space constraints
-*/
-//BRUTE FORCE
-//{a:2, b:3} key = char, val = freq by 1 pass through string
-//minumum = -1
-//go through original string and match that with frequency map aka attempt to walk through original string and return the first character with the value of 1
-
-function singularOccurence (str) {
-    let chars = {}
-    for(char of str) {
-        if (chars[char]) chars[char]++
-        else chars[char] = 1
-    }
-    for (char of str) {
-        if (chars[char] === 1) return char
-    }
-    return -1
-}
-//time = 2n
-//space = n
-
-//OPTIMAL
-function singularOccurence (str) {
-    //min variable you only care about the 
-
-}
-
-console.log(singularOccurence('bababcdddef'))
-                                           
-map with charCodeAt-offset bc indices is 0 Staart
-single pass bc only going through 26 char, the pass is for frequency and then loop through keys
-it's been hard for me to come up with examples for this but here's a scenario that i've seen before and i saw them go throuhg a dispute, this is how some people respond and this is how i would respond.
