5be8f5b46bc45da2dec2b279dba079ac4432866a
dongb909 <dongb909@gmail.com>
2020-06-03 23:05:57 -0700

    meetingRoom1

--- arrays/meetingRoomI.js
+++ arrays/meetingRoomI.js
@@ -12,6 +12,33 @@ Output: true
 NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
  */
 
+// var canAttendMeetings = function(intervals) {
+//   if(intervals.length === 0 || intervals[0].length === 0 || !intervals) return true
+//   intervals.sort((a,b) => a[0] - b[0])
+//   let [prevStart, prevEnd] = intervals[0]
+//   for(let i = 1; i<intervals.length; i++){
+//       let [currStart, currEnd] = intervals[i]
+//       if(currStart < prevEnd) return false
+//       [prevStart, prevEnd] = intervals[i] //MUST DO THIS FOR NEXT ITERATION
+//   }
+// return true  
+// };
+
+
+
+
+// OR 
+
 var canAttendMeetings = function(intervals) {
-    
+  intervals.sort(function(a, b) {
+      return a[0] - b[0]
+  })
+  for (var i = 0; i < intervals.length - 1; i++) {
+      if (intervals[i][1] > intervals[i + 1][0]) {
+          return false;
+      }
+  }
+  return true;
 };
+
+console.log(canAttendMeetings([[0,2],[5,10],[15,20]]))
--- arrays/meetingRoomII.js
+++ arrays/meetingRoomII.js
@@ -13,5 +13,19 @@ NOTE: input types have been changed on April 15, 2019. Please reset to default c
 */
 
 var minMeetingRooms = function(intervals) {
-    
+  if(intervals.length === 0 || intervals[0].length === 0 || !intervals) return []
+  console.log(intervals)
+  intervals.sort((a,b) => a[0] - b[0])
+  console.log(intervals)
+  let rooms = 1
+  for(let i = 0; i < intervals.length-1; i++){
+      if (intervals[i][1] > intervals[i+1][0]) rooms++
+      console.log(intervals[i][1], intervals[i+1][0])
+  }
+  return rooms
 };
+
+console.log(minMeetingRooms(
+  
+[[9,10],[4,9],[4,17]]
+))
