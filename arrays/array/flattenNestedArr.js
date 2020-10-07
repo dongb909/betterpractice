b912d42111b821d76992b237f7685b3050031ad8
dongb909 <dongb909@gmail.com>
2020-10-07 04:39:22 -0700

    2 sum

--- arrays/array/flattenNestedArr.js
+++ arrays/array/flattenNestedArr.js
@@ -1,21 +1,32 @@
 //[[[]]]
 //[1,2,[3,4],[5,[6,[7], 8]]]
 
-let flatten = (arr) => {
+//RECURSIVE
+// let flatten = (arr) => {
+//   let result = [];
+//   for(let el of arr) {
+//     if (Array.isArray(el)) {
+//       result = result.concat(flatten(el));
+//     } else {
+//       result.push(el)
+//     }
+//   }
+//   return result;
+// }
+
+function flatten(arr) {
   let result = [];
-  for(let el of arr) {
+  for (const el of arr) {
+    // if(Array.isArray(el)) result.push(flatten(el)) //NOOOOO DO NOT PUSH BECAUSE YOU'RE RETURNING ANOTHER ARRAY AT EACH RECURSION!!
+    // else result.push(el)
     if (Array.isArray(el)) {
-      result = result.concat(flatten(el));
-    } else {
-      result.push(el)
-    }
+      result = result.concat(flatten(el)); //MUST RESET RESULT ASSIGNMENT BECAUSE 'CONCAT' ALWAYS RETURNS A NEW ARRAY OR STRING
+    } else result.push(el); //can push here bc this el is not an arr
   }
   return result;
 }
+console.log(flatten([1, 2, [3, 4], [5, [6, [7], 8]]]));
 
-
-console.log(flatten([1,2,[3,4],[5,[6,[7], 8]]]))
-
-let i = [1,2]
-let j = [3,4]
-// console.log(i.concat(...j))
+let i = [1, 2];
+let j = [3, 4];
+// console.log(i.concat(j))
--- arrays/array/twoSums.js
+++ arrays/array/twoSums.js
@@ -12,7 +12,7 @@ return [0, 1].
 
 //horrible 160ms first attempt
 // var twoSum = function(nums, target) {
-//   let index2; 
+//   let index2;
 //   for (let i = 0; i<nums.length; i++) {
 //     index2 = nums.indexOf(target - nums[i]) //slow because index of is also On
 //     if((index2!== i) && (index2!== -1)) {
@@ -71,25 +71,49 @@ const twoSum = (nums, target) => {
 //       }
 //         dic[nums[i]] = i;
 //     }
-//  }; 
+//  };
 //  //////////////////////////////////////////////////////////////
 
+//DO NOT USE SET!!!
+// function twoSum2(arr, targ) {
+//   let seen = new Set()
+//   let diff
+//   for(const num of arr){
+//     diff = targ - num
+//     if (seen.has(diff)) return [seen.get(diff), num] //NO CANNOT USE SET HERE FOR THIS BC YOU CANNOT '.GET' VALUE DIRECTLY, YOU CAN DO SET.VALUES AND ITEREATE TO FIND YOUR VALUE, THAT'S IT
+//     seen.add(num)
+//   }
+//   return null
+// }
+// console.log(twoSum2([3,2,4],6))
 
+//USING MAP
+function twoSum2(arr, targ) {
+  let seen = new Map(); //better to use MAP than obj literals bc it's cleaner
+  let diff;
+  for (const num of arr) {
+    diff = targ - num;
+    // if (diff in seen){ //NO CANNOT do this if using MAP, can do if using obj
+    if (seen.has(diff)) {
+      return [seen.get(diff), num];
+    } //NO CANNOT do this if using MAP, can do if using obj
+    seen.set(num, num); //doesn't matter what you set the value you be bc you don't care about the indices, just the key themselves so cheating in creating a set using a map
+  }
+  return seen;
+}
+console.log(twoSum2([3, 2, 4], 6));
 
-
-
+//USING OBJ LITERALS
 let twoSum = (nums, target) => {
   let map = {};
   //go through all indices and add to map if over, then compare to see if the diff in is map
-  for(let i in nums) { //keys of arrays are indices so setting indices to i aka keys to i
+  for (let i in nums) {
+    //keys of arrays are indices so setting indices to i aka keys to i
     // if (target-nums[i] === map[nums[i]] ){
-    if (target-nums[i] in map){
-      return [map[target-nums[i]], i]
+    if (target - nums[i] in map) {
+      return [map[target - nums[i]], i];
     }
-    map[nums[i]] = i
+    map[nums[i]] = i;
   }
   return null;
-}
-
-console.log(twoSum([3,2,4],
-  6))
+};
