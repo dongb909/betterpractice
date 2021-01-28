e836df605d0c1a8a6f57c59b544c80cb29925415
dongb909 <dongb909@gmail.com>
2021-01-27 16:02:57 -0800

    Complete valid tree graph

--- graphs js/isValidTreeGraph.js
+++ graphs js/isValidTreeGraph.js
@@ -8,11 +8,58 @@ input: n=5, edges = [[0,1],[0,2],[0,3],[1,4]]
 output: true
 
 ex2:
-input: n=5, edges = [[0,1],[1,2],[2,3],[1,3], [1,4]]
-output: true
+input: n=5, edges = [[0,1],[1,2],[2,3],[1,3],[1,4]]
+output: false
 Can assume that no duplicate edges will appear. so there's not [0,1] then [1,0]
 
 edge cases: when edges >= vertices, if correct number of edges but one edge forms a cycle and thus leaves an unreachable node without an edge connecting it to the rest of the tree, if there's less edges than we need, if no tree at all
 */
 
-const validTreeGraph = () => {};
+const validTreeGraph = (n, edges) => {
+  //if there's no tree at all
+  if (!n || !edges.length) return false;
+  //if more or equaledges than nodes //if less edges than we need
+  if (edges.length >= n || edges.length < n - 1) return false;
+  //create adj list
+  const adj = new Array(n).fill(null).map(() => new Array());
+  edges.forEach(([a, b]) => {
+    adj[a].push(b);
+    adj[b].push(a);
+  });
+  console.log(adj);
+  const seen = new Set([0]); //already on 0 so add it to seen
+  const q = [0]; //just start at 0, even if root is not zero, edges are undirected so you'll end up there regardless
+  while (q.length) {
+    let curr = q.shift();
+    seen.add(curr);
+    for (let nei of adj[curr]) {
+      if (!seen.has(nei)) {
+        //only process if haven't seen , if seen then don't do anything
+        // seen.add(nei)
+        q.push(nei);
+      }
+    }
+  }
+  console.log(seen.size, n);
+  return seen.size === n;
+};
+
+console.assert(
+  validTreeGraph(5, [
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [1, 4],
+  ]) === true,
+  "Should return true"
+);
+console.assert(
+  validTreeGraph(5, [
+    [0, 1],
+    [1, 2],
+    [2, 3],
+    [1, 3],
+    [1, 4],
+  ]) === false,
+  "Should return false bc there's a cycle"
+);
